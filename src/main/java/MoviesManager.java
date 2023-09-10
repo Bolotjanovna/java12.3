e216c02868a2e47fdfa9b129a3d55a994c7da138
Bolotjanovna <toksonbaeva2104@gmail.com>
2023-09-10 15:47:03 +0500

    testing Films

--- src/main/java/MoviesManager.java
+++ src/main/java/MoviesManager.java
@@ -7,13 +7,13 @@ public class MoviesManager {
         this.limit = 7;
     }
 
-//    public void addMovie(String movie) {
-//        String[] tmp = new String[movies.length + 1];
-//        for (int i = 0; i < movies.length; i++) {
-//            tmp[i] = movies[i];
-//        }
-//        tmp[tmp.length - 1] = movie;
-//        movies = tmp;
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
     }
 
     public String[] findAll() {
@@ -28,13 +28,13 @@ public class MoviesManager {
             resultlength = limit;
         }
 
-            String[] tmp = new String[resultlength];
-            for (int i = 0; i < tmp.length; i++) {
-                tmp[i] = movies[movies.length - 1 - i];
-            }
-            return tmp;
+        String[] tmp = new String[resultlength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
         }
+        return tmp;
     }
+}
 
 
 
